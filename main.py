94dc44d5da82458e5fd41fe725d2102ceb83103c
Ankur Gajurel <66795857+ankurgajurel@users.noreply.github.com>
2021-09-27 19:37:03 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-"Please don't edit this file"
+nums = ['+9779818542902']
 
-import requests
-from lxml import html
+scrips = ["ICFC", "SHPC", "NIFRA", "NRIC", "SHEL", "CCBL", "CZBIL", "RRHP", "RHPC", "RHPL", "SPDL"]
 
-def mainFn():
-    scrip = input("Enter the company symbol: ")
-    target = float(input("Enter you target price: "))
-    url = "https://www.nepsealpha.com/search?q=" + scrip
-    r = requests.get(url)
-    hellYea = html.fromstring(r.content)
-    price = float(hellYea.xpath("/html/body/div/div[4]/div/div/div/div/div/div[1]/div/div[2]/div/div[2]/div/table/thead/tr/th/h2/text()")[1].strip())
+import sendSMS
 
-    while True:        
-        if price > (target - 1):
-            from plyer import notification 
-            title = "The target of " + scrip + " has been reached"
-            message = "The price of " + scrip + " has reached " + str(price)
-            notification.notify(title = title, message = message)
-
-            from twilio.rest import Client
-            client = Client("AC0f742cb0a43176b34f73d596b5f03846", "f43f184c5ab7f08a3c3c082c3197b24f")
-            client.messages.create(to = ["+9779818542902"], from_ = "+12019776138", body = message)
-            break
-        else:
-            pass
-               
-
-mainFn()
-    
+sendSMS.sendSMS()
